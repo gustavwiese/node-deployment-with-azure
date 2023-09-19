6d619e9a5c769677a9f73e83b8f8c1f0e7975ace
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-09-19 19:00:19 +0200

    Update db-connect.js

--- db-connect.js
+++ db-connect.js
@@ -1,16 +1,23 @@
 import mysql from "mysql2"; // using mysql2 - installed npm library
 import "dotenv/config";
+import fs from "fs/promises";
 
 // using the variables from the .env file
 // and creates the connection to database
-const dbConnection = mysql.createConnection({
+const dbConfig = {
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     database: process.env.MYSQL_DATABASE,
     password: process.env.MYSQL_PASSWORD,
     multipleStatements: true
-});
+};
+
+if (process.env.MYSQL_CERT) {
+    dbConfig.ssl = { cs: fs.readFile("DigiCertGlobalRootCA.crt.pem") };
+}
+
+const dbConnection = mysql.createConnection(dbConfig);
 
 // exports database connection
 export default dbConnection;
