a670ff6e830bfb08f138135954967e0fa40e1968
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-09-19 23:05:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json()); // to parse JSON bodies
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Node Express REST Users API");
+    res.send("Node Express REST Users API 🎉");
 });
 
 // READ all users
